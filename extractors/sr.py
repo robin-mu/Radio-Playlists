2ec39737e93de02e2cdfc8beb626a5a65b590ca3
robin-mu <robin-mu@users.noreply.github.com>
2026-04-29 23:45:30 +0200

    sr2 returns broken html

--- extractors/sr.py
+++ extractors/sr.py
@@ -18,7 +18,7 @@ class SrExtractor(PlaylistExtractor):
         return pd.date_range(start, end, freq='1h')
 
     def get_url(self, station: str, time):
-        date_offset = (pd.Timestamp.now().floor('1d') - time.floor('1d')).days
+        date_offset = (pd.Timestamp.now().floor('1D') - time.floor('1D')).days
         hour = time.hour
 
         return f'https://musikrecherche.sr.de/{station}/musicresearch.php?DateSelect={date_offset}&TimeSelect={hour}', {}
@@ -45,31 +45,66 @@ class SrExtractor(PlaylistExtractor):
                 dtype=str)
         else:
             df = pd.DataFrame()
-            for time, content in zip(soup.find_all(class_='musicResearch__Item__Time'),
-                                     soup.find_all(class_='musicResearch__Item__Content')):
-                if content.find(class_='background-title'):
-                    continue
 
-                titles = content.find_all(class_='musicResearch__Item__Content__Title')
-                if len(titles) == 1:
-                    composer = ''
-                    title = titles[0].text
-                elif len(titles) == 2:
-                    composer = titles[0].text
-                    title = titles[1].text
-                else:
-                    self.logger.warning(f'More than 2 composers for {date}', extra=log_extra)
-                    composer = ''
-                    title = ''
+            title_lengths = [len(content.find_all(class_='musicResearch__Item__Content__Title')) for content in soup.find_all(class_='musicResearch__Item__Content')]
+            if any([x > 2 for x in title_lengths]):
+                # incorrect html
+                info = soup.find_all(class_=['musicResearch__Item__Time', 'musicResearch__Item__Content__Title',
+                                             'musicResearch__Item__Content__Artist'])
 
+                time = None
+                composer = ''
+                title = ''
                 artists = []
-                for artist in content.find_all(class_='musicResearch__Item__Content__Artist'):
-                    if ':' in artist.text:
-                        break
-                    artists.append(artist.text.strip())
+                for tag in info:
+                    if 'musicResearch__Item__Time' in tag.attrs['class']:
+                        if time is not None:
+                            date = pd.to_datetime(date.strftime('%Y%m%d') + ' ' + time, format='%Y%m%d %H:%M')
+                            df = pd.concat(
+                                [df, pd.DataFrame({'composer': composer, 'title': title, 'artist': '; '.join(artists)},
+                                                  index=pd.Series(date, name='time'))])
 
-                date = pd.to_datetime(date.strftime('%Y%m%d') + ' ' + time.text, format='%Y%m%d %H:%M')
-                df = pd.concat([df, pd.DataFrame({'composer': composer, 'title': title, 'artist': '; '.join(artists)},
-                                                 index=pd.Series(date, name='time'))])
+                        time = tag.text
+                        composer = ''
+                        title = ''
+                        artists = []
+                    elif 'musicResearch__Item__Content__Title' in tag.attrs['class']:
+                        if title:
+                            composer = title
+                        title = tag.text
+                    elif 'musicResearch__Item__Content__Artist' in tag.attrs['class']:
+                        if ':' in tag.text:
+                            continue
+                        artists.append(tag.text.strip())
+
+            else:
+                self.logger.warning(f'Correct HTML returned for {date}', extra=log_extra)
+
+                for time, content in zip(soup.find_all(class_='musicResearch__Item__Time'),
+                                         soup.find_all(class_='musicResearch__Item__Content')):
+                    if content.find(class_='background-title'):
+                        continue
+
+                    titles = content.find_all(class_='musicResearch__Item__Content__Title')
+                    if len(titles) == 1:
+                        composer = ''
+                        title = titles[0].text
+                    elif len(titles) == 2:
+                        composer = titles[0].text
+                        title = titles[1].text
+                    else:
+                        self.logger.warning(f'More than 2 composers for {date}', extra=log_extra)
+                        composer = ''
+                        title = ''
+
+                    artists = []
+                    for artist in content.find_all(class_='musicResearch__Item__Content__Artist'):
+                        if ':' in artist.text:
+                            break
+                        artists.append(artist.text.strip())
+
+                    date = pd.to_datetime(date.strftime('%Y%m%d') + ' ' + time.text, format='%Y%m%d %H:%M')
+                    df = pd.concat([df, pd.DataFrame({'composer': composer, 'title': title, 'artist': '; '.join(artists)},
+                                                     index=pd.Series(date, name='time'))])
 
         return df
